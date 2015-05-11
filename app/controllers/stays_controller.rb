82d4a6d59eca0bf941eb628253cbbc3e8d8e176b
RianaFerreira <rianaferreira@gmail.com>
2015-05-11 12:39:27 +1000

    wip

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -1,15 +1,38 @@
 class StaysController < ApplicationController
+  def index
+    @stays = Stay.all
+  end
+
   def new
     @stay = Stay.new
   end
 
   def create
-    Stay.create(stay_params)
+    stay = Stay.new(stay_params)
+    if stay.save!
+      flash[:notice] = 'Stay created.'
+      redirect_to 'index'
+    else
+      flash[:error] = 'Failed to create stay.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    stay = Stay.find(params[:id])
+
+    if stay.destroy
+      flash[:notice] = "Successfully removed stay."
+    else
+      flash[:error] = "Failed to remove stay."
+    end
+
+    redirect_to stays_path
   end
 
   private
 
   def stay_params
-    params.require(:stay).permit(:entry_date, :exit_date)
+    params.require(:stay).permit(:entry_date, :exit_date, :time_to_qualify)
   end
 end
